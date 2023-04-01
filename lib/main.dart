736c431f0b52a2bd51e223e91173ec345c5429be
fatma010932 <129009470+fatma010932@users.noreply.github.com>
2023-04-01 23:19:47 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(appBar: AppBar(
+      title: Text('First Application'),
+      backgroundColor: Colors.Red[800],
+    ),
+    body: Center(
+      child: Text('Fatma Mamdouh'),
+    ),
+    ),
+  ));
+}
+
+
